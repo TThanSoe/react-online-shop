602271b9d3d2e07a253913c7e0e6f3518224c26f
thanthan <tsoe@and.digital>
2022-07-11 13:46:09 +0100

    added sign in and sign out

--- src/store/root-reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-
-// import { userReducer } from "./user/user.reducer";
-import { navigationReducer } from "./navigation/navigation.reducer";
-
-export const rootReducer = combineReducers({
-  navigation: navigationReducer,
-});
